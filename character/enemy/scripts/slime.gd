84a0437e478440f28e3f14afb41e90842ada1aac
hafiy-hz <hafiyal2006@gmail.com>
2025-04-20 22:41:12 +0800

    test

--- character/enemy/scripts/slime.gd
+++ character/enemy/scripts/slime.gd
@@ -4,7 +4,14 @@ var speed = 40
 var player_chase = false
 var player = null
 
+var health = 100
+var player_in_attack_zone = false
+
+
 func _physics_process(delta):
+	
+	deal_with_damage()
+	
 	if player_chase:
 		position += (player.position - position)/speed
 		
@@ -30,3 +37,21 @@ func _on_detection_area_body_exited( body ):
 	
 func enemy():
 	pass
+	
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_attack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_attack_zone = false
+		
+func deal_with_damage():
+	if player_in_attack_zone and global.player_current_attack == true:
+		health = health - 20
+		print("slime health =", health)
+		if health <= 0:
+			self.queue_free()
