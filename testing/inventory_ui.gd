91f45b232e6e0a220513c38cd12c3464e7c0530c
Haikal-Hazim10 <haikalhazeem72@gmail.com>
2025-05-03 22:04:28 +0800

    Update

--- testing/inventory_ui.gd
+++ testing/inventory_ui.gd
@@ -12,23 +12,41 @@ func _ready():
         # You would normally set this based on your actual inventory
         slot.set_meta("item_data", {
             "name": "Sword of Light",
+            "type": "Sword",
+            "rarity": "Legendary",
             "power": 120
+    
         })
+    
     # Start with InfoPanel hidden
     $InfoPanel.visible = false
 
 func _on_slot_mouse_entered(slot):
-    var item_data = slot.get_meta("item_data")
-    if item_data:
-        hovering_item = true
-        $InfoPanel.visible = true
-        $Infolabel.text = "Name: " + item_data["name"] + "\nPower: " + str(item_data["power"])
+    print("Mouse entered slot")
+    if slot.has_meta("item_data"):
+        var item_data = slot.get_meta("item_data")
+        if item_data:
+            hovering_item = true
+            $InfoPanel.visible = true
+            
+            # Format tooltip with all item properties
+            var tooltip_text = ""
+            tooltip_text += "Name: " + item_data["name"] + "\n"
+            tooltip_text += "Type: " + item_data["type"] + "\n"
+            tooltip_text += "Rarity: " + item_data["rarity"] + "\n"
+            tooltip_text += "Power: " + str(item_data["power"])
+            
+            $InfoPanel/Infolabel.text = tooltip_text
+    else:
+        print("No item_data found on this slot")
 
-func _on_slot_mouse_exited(slot):
+func _on_slot_mouse_exited(_slot):
+    print("Mouse exited slot")
     hovering_item = false
     $InfoPanel.visible = false
 
 func _process(delta):
+    # Update tooltip position when hovering over an item
     if hovering_item:
         var mouse_pos = get_viewport().get_mouse_position()
         var target_pos = mouse_pos + Vector2(20, 20)
