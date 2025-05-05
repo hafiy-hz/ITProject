3670f3075dee6a845f660e9a9260dd77feeb042f
ZNnaim89 <zairul060323@gmail.com>
2025-05-05 09:34:33 +0800

    heart hud

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -4,15 +4,24 @@ var cardinal_direction : Vector2 = Vector2.DOWN
 const DIR_4 = [ Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
 var direction : Vector2 = Vector2.ZERO
 
+var invulnerable : bool = false
+var hp : int = 6
+var max_hp : int = 6
+
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var sprite : Sprite2D = $Sprite2D
+@onready var hit_box : HitBox = $HitBox
 @onready var state_machine: PlayerStateMachine = $StateMachine
 
 signal DirectionChanged( new_direction: Vector2 )
+signal player_damaged( hurt_box: HurtBox)
 
 func _ready():
+	PlayerManager.player = self
 	state_machine.Initialize(self)
+#	hit_box.damaged.connect( _take_damage )
+	update_hp(99)
 	pass
 
 
@@ -31,7 +40,7 @@ func _physics_process( _delta ):
 	move_and_slide()
 
 
-func SetDirection() -> bool:
+func Set_direction() -> bool:
 	if direction == Vector2.ZERO:
 		return false
 	
@@ -62,3 +71,19 @@ func AnimDirection() -> String:
 	else:
 		return "side"
 		
+
+func _take_damage( hurt_box : HurtBox ) -> void:
+	if invulnerable == true:
+		return
+	update_hp( -hurt_box.damage )
+	if hp > 0:
+		player_damaged.emit( hurt_box )
+	else:
+		player_damaged.emit( hurt_box )
+		update_hp(99)
+	pass
+
+func update_hp( delta : int ) -> void:
+	hp = clampi( hp + delta, 0, max_hp )
+	PlayerHud.update_hp( hp, max_hp )
+	pass
--- Player/Scripts/state_idle.gd
+++ Player/Scripts/state_idle.gd
@@ -1,37 +1,26 @@
-class_name State_Idle extends State
+extends State
+
+@export var move_speed : float = 100.0
 
 @onready var walk: State_Walk = $"../Walk"
 @onready var attack: State_Attack = $"../Attack"
 
-
-## what happend when the player enter this state?
-func Enter() -> void: 
+func Enter() -> void:
 	player.UpdateAnimation("idle")
-	
-	pass
-
 
 func Exit() -> void:
-	
 	pass
 
-## what happen during _process update in this state
-func Process( _delta : float ) -> State:
+func Process(_delta: float) -> State:
 	if player.direction != Vector2.ZERO:
 		return walk
 	player.velocity = Vector2.ZERO
 	return null
 
-
-
-
-## what happend during the _pyshics_process update in this state
-func Physics( _delta : float ) -> State:
+func Physics(_delta: float) -> State:
 	return null
 
-
-## what happend with input event in this state
-func HandleInput( _event: InputEvent ) -> State:
+func HandleInput(_event: InputEvent) -> State:
 	if _event.is_action_pressed("attack"):
 		return attack
 	return null
