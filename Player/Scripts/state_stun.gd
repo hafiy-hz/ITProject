ecb1dcf27c4e09edbcbb883ec3d09eccd1596488
ZNnaim89 <zairul060323@gmail.com>
2025-05-12 02:13:39 +0800

    animation_update

---
+++ Player/Scripts/state_stun.gd
@@ -0,0 +1,55 @@
+class_name State_Walk extends State
+
+@export var knockback_speed : float = 200.0
+@export var decelerate_speed : float = 10
+@export var invulnerable_duration : float = 1.0
+
+var hurt_box : HurtBox
+var direction : Vector2
+
+var next_state : State = null
+
+@onready var idle: State_Idle = $"../Idle"
+
+func init() -> void:
+	player.player_damaged.connect( _player_damaged )
+
+
+
+
+## what happend when the player enter this state?
+func Enter() -> void: 
+	player.update_animation("stun")
+	player.animation_player.animation_finished.connect( _animation_finished )
+	
+	direction = player.global_position.direction_to( hurt_box.global_position )
+	pass
+
+
+func Exit() -> void:
+	next_state = null
+	player.animation_player.animation_finished.disconnect( _animation_finished )
+	pass
+
+## what happen during _process update in this state
+func Process( _delta : float ) -> State:
+	return next_state
+
+
+## what happend during the _pyshics_process update in this state
+func Physics( _delta : float ) -> State:
+	return null
+
+
+## what happend with input event in this state
+func HandleInput( _event: InputEvent ) -> State:
+	return null
+
+
+func _player_damaged( _hurt_box : HurtBox ) -> void:
+	hurt_box = _hurt_box
+	state_machine.change_state( self )
+	pass
+
+func _animation_finished( _a: String ) -> void:
+	next_state = idle
