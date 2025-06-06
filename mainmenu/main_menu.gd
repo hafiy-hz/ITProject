517d76515329b0df2b3e61dfc2703f1c5b372315
hafiy-hz <hafiyal2006@gmail.com>
2025-06-06 18:44:48 +0800

    CAMERA AND MAIN MENU FIXES

--- mainmenu/main_menu.gd
+++ mainmenu/main_menu.gd
@@ -1,24 +1,27 @@
 extends Control
 
 func _ready() -> void:
-    scale = Vector2(0.5, 0.5)  # Counteracts scale 4
+	scale = Vector2(0.5, 0.5)  # Counteracts scale 4
+	 # Make sure we're not in game mode
+	PlayerManager.is_in_game = false
+	PlayerManager.player_spawn = false
    
 
 func _on_startbutton_pressed() -> void:
-    get_tree().change_scene_to_file("res://mainmenu/PlayerUsername.tscn")
+	get_tree().change_scene_to_file("res://mainmenu/PlayerUsername.tscn")
 
 func _on_options_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://mainmenu/options.tscn")
+	get_tree().change_scene_to_file("res://mainmenu/options.tscn")
 
 func _on_exitbutton_pressed() -> void:
-    get_tree().quit()
-    
+	get_tree().quit()
+	
 func _process(delta):
-    position += (get_global_mouse_position()*delta)-position
-    
+	position += (get_global_mouse_position()*delta)-position
+	
 
-    
-    
+	
+	
 
 
-    
+	
--- stage/scripts/level.gd
+++ stage/scripts/level.gd
@@ -1,12 +1,12 @@
-class_name Level1 extends Node2D
+class_name Level extends Node2D
 
 func _ready() -> void:
-    self.y_sort_enabled = true 
-    PlayerManager.set_as_parent( self )
-    LevelManagers.level_load_started.connect( _free_level )
+	self.y_sort_enabled = true 
+	PlayerManager.set_as_parent( self )
+	LevelManagers.level_load_started.connect( _free_level )
 
 
 func _free_level() -> void:
-    PlayerManager.unparent_player( self )
-    queue_free()
-    
+	PlayerManager.unparent_player( self )
+	queue_free()
+	
