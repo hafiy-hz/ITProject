e80268bfbf0872338774e812d565d7038a273676
Haikal-Hazim10 <haikalhazeem72@gmail.com>
2025-06-09 10:50:16 +0800

    Update

--- GUI/pause_menu/inventory/scripts/inventory_data.gd
+++ GUI/pause_menu/inventory/scripts/inventory_data.gd
@@ -5,79 +5,79 @@ class_name InventoryData extends Resource
 
 
 func _init() -> void:
-	connect_slots()
-	pass
+    connect_slots()
+    pass
 
 
 
 func add_item( item : ItemsData, count : int = 1 ) -> bool:
-	for s in slots:
-		if s:
-			if s.item_data == item:
-				s.quantity += count
-				return true
-	
-	for i in slots.size():
-		if slots[ i ] == null:
-			var new = SlotData.new()
-			new.item_data = item
-			new.quantity = count
-			slots[ i ] = new
-			new.changed.connect( slot_changed )
-			return true
-	
-	print("Inventory was full!!")
-	return false
+    for s in slots:
+        if s:
+            if s.item_data == item:
+                s.quantity += count
+                return true
+    
+    for i in slots.size():
+        if slots[ i ] == null:
+            var new = SlotData.new()
+            new.item_data = item
+            new.quantity = count
+            slots[ i ] = new
+            new.changed.connect( slot_changed )
+            return true
+    
+    print("Inventory was full!!")
+    return false
 
 
 func connect_slots() -> void:
-	for s in slots:
-		if s:
-			s.changed.connect( slot_changed )
-	
+    for s in slots:
+        if s:
+            s.changed.connect( slot_changed )
+    
 
 func slot_changed() -> void:
-	for s in slots:
-		if s:
-			if s.quantity < 1:
-				s.changed.disconnect( slot_changed )
-				var index = slots.find( s )
-				slots[ index ] = null
-				emit_changed()
-	pass
+    for s in slots:
+        if s:
+            if s.quantity < 1:
+                s.changed.disconnect( slot_changed )
+                var index = slots.find( s )
+                slots[ index ] = null
+                emit_changed()
+    pass
 
 
 ## gather inventory into an array
 func get_save_data() -> Array:
-	var item_save : Array = []
-	for i in slots.size():
-		item_save.append( item_to_saved( slots[i] ) )
-	return item_save
+    var item_save : Array = []
+    for i in slots.size():
+        item_save.append( item_to_saved( slots[i] ) )
+    return item_save
 
 
 ## convert each inventory item to a dictionary
 func item_to_saved( slot : SlotData ) -> Dictionary:
-	var result = { item = "", quantity = 0 }
-	if slot != null:
-		result.quantity = slot.quantity
-		if slot.item_data != null:
-			result.item = slot.item_data.resource_path
-	return result
+    var result = { item = "", quantity = 0 }
+    if slot != null:
+        result.quantity = slot.quantity
+        if slot.item_data != null:
+            result.item = slot.item_data.resource_path
+    return result
 
 func parse_save_data( save_data : Array ) -> void:
-	var array_size = slots.size()
-	slots.clear()
-	slots.resize( array_size )
-	for i in save_data.size():
-		slots[ i ] = item_from_save( save_data[ i ] )
-	connect_slots()
+    var array_size = slots.size()
+    slots.clear()
+    slots.resize( array_size )
+    for i in save_data.size():
+        slots[ i ] = item_from_save( save_data[ i ] )
+    connect_slots()
 
 
 func item_from_save( save_object : Dictionary ) -> SlotData:
-	if save_object.item == "":
-		return null
-	var new_slot : SlotData = SlotData.new()
-	new_slot.item_data = load( save_object.item )
-	new_slot.quantity = int( save_object.quantity )
-	
-	return new_slot
+    if save_object.item == "":
+        return null
+    var new_slot : SlotData = SlotData.new()
+    new_slot.item_data = load( save_object.item )
+    new_slot.quantity = int( save_object.quantity )
+    
+    return new_slot
