7b446918c1ddc4dbb4d4a5942697594b2d6f91a4
hafiy-hz <hafiyal2006@gmail.com>
2025-06-14 05:09:50 +0800

    asd

--- mainmenu/player_username.gd
+++ mainmenu/player_username.gd
@@ -10,7 +10,7 @@ func _on_button_rapier_pressed() -> void:
 	PlayerManager.player_class = "rapier"
 	PlayerManager.player_damage = PlayerManager.class_damage["rapier"]
 	PlayerManager.player_info_updated.emit()
-	get_tree().change_scene_to_file("res://stage/level_2.tscn")
+	get_tree().change_scene_to_file("res://stage/level_1.tscn")
 
 
 func _on_button_sword_pressed() -> void:
@@ -19,7 +19,7 @@ func _on_button_sword_pressed() -> void:
 	PlayerManager.player_class = "sword"
 	PlayerManager.player_damage = PlayerManager.class_damage["sword"]
 	PlayerManager.player_info_updated.emit()
-	get_tree().change_scene_to_file("res://stage/level_2.tscn")
+	get_tree().change_scene_to_file("res://stage/level_1.tscn")
 
 
 func _on_button_great_sword_pressed() -> void:
@@ -28,7 +28,7 @@ func _on_button_great_sword_pressed() -> void:
 	PlayerManager.player_class = "greatsword"
 	PlayerManager.player_damage = PlayerManager.class_damage["greatsword"]
 	PlayerManager.player_info_updated.emit()
-	get_tree().change_scene_to_file("res://stage/level_2.tscn")
+	get_tree().change_scene_to_file("res://stage/level_1.tscn")
 
 
 func _on_back_button_2_pressed() -> void:
--- stage/dark_wizard/script/energy_beam_sprite.gd
+++ stage/dark_wizard/script/energy_beam_sprite.gd
@@ -12,32 +12,3 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	region_rect.position += Vector2( speed * delta, 0 )
-
-#@export var speed : float = 450
-#@export var damage : int = 10  # Damage amount
-
-#var rect : Rect2
-#var can_damage : bool = false
-
-#func _ready() -> void:
-	#rect = self.region_rect
-	# Add to the "enemy_attack" group for collision detection
-	#add_to_group("enemy_attack")
-
-#func _process(delta: float) -> void:
-	#mregion_rect.position += Vector2( speed * delta, 0 )
-	
-	# Only check for damage when the beam is visible
-	#if visible and can_damage:
-		#check_player_collision()
-
-#func check_player_collision() -> void:
-	#var bodies = get_overlapping_bodies()
-	#for body in bodies:
-		#if body.is_in_group("player"):
-			#if body.has_method("take_damage"):
-				#body.take_damage(damage)
-				# Optional: Add a cooldown to prevent multiple hits
-				#can_damage = false
-				#await get_tree().create_timer(0.5).timeout
-				#can_damage = true
