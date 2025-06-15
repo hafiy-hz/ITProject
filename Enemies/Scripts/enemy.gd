f23ebca41feb8863fab47bf2d749475276cdcb86
hafiy-hz <hafiyal2006@gmail.com>
2025-06-15 21:57:58 +0800

    healling

--- Enemies/Scripts/enemy.gd
+++ Enemies/Scripts/enemy.gd
@@ -20,63 +20,63 @@ var invulnerable : bool = false
 @onready var state_machine : EnemyStateMachine = $EnemyStateMachine
 
 func _ready():
-    state_machine.initialize( self )
-    player = PlayerManager.player
-    hit_box.damaged.connect( _take_damage )
-    pass
+	state_machine.initialize( self )
+	player = PlayerManager.player
+	hit_box.damaged.connect( _take_damage )
+	pass
 
 func _process(_delta):
-    pass
+	pass
 
 func _physics_process(_delta):
-    move_and_slide()
+	move_and_slide()
 
 func set_direction( _new_direction : Vector2 ) -> bool:
-    direction = _new_direction
-    if direction == Vector2.ZERO:
-        return false
-    
-    var direction_id : int = int( round(
-            ( direction + cardinal_direction * 0.1 ).angle()
-            / TAU * DIR_4.size()
-    ))
-    var new_dir = DIR_4[ direction_id ]
-    
-    if new_dir == cardinal_direction:
-        return false
-    
-    cardinal_direction = new_dir
-    direction_changed.emit( new_dir )
-    sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
-    return true
+	direction = _new_direction
+	if direction == Vector2.ZERO:
+		return false
+	
+	var direction_id : int = int( round(
+			( direction + cardinal_direction * 0.1 ).angle()
+			/ TAU * DIR_4.size()
+	))
+	var new_dir = DIR_4[ direction_id ]
+	
+	if new_dir == cardinal_direction:
+		return false
+	
+	cardinal_direction = new_dir
+	direction_changed.emit( new_dir )
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true
 
 func update_animation( state : String ) -> void:
-    animation_player.play( state + "_" + anim_direction() )
-    pass
+	animation_player.play( state + "_" + anim_direction() )
+	pass
 
 
 func anim_direction() -> String:
-    if cardinal_direction == Vector2.DOWN:
-        return "down"
-    elif cardinal_direction == Vector2.UP:
-        return "up"
-    else:
-        return "side"
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
 
 func _take_damage( hurt_box : HurtBox ) -> void:
-    if invulnerable == true:
-        return
+	if invulnerable == true:
+		return
 
-    hp -= hurt_box.damage
-    enemy_damaged.emit()
+	hp -= hurt_box.damage
+	enemy_damaged.emit()
 
-    # Show floating damage number
-    var floating_text = preload("res://Enemies/floating_text.tscn").instantiate()
-    floating_text.position = global_position + Vector2(0, -20)
-    floating_text.initial_text = str(hurt_box.damage)
-    get_tree().current_scene.add_child(floating_text)
+	# Show floating damage number
+	var floating_text = preload("res://Enemies/floating_text.tscn").instantiate()
+	floating_text.position = global_position + Vector2(0, -20)
+	floating_text.initial_text = str(hurt_box.damage)
+	get_tree().current_scene.add_child(floating_text)
 
-    if hp > 0:
-        enemy_damaged.emit( hurt_box )
-    else:
-        enemy_destroyed.emit( hurt_box )
+	if hp > 0:
+		enemy_damaged.emit( hurt_box )
+	else:
+		enemy_destroyed.emit( hurt_box )
